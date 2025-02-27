933acd42b9f57687d12ae579bc33c816bc3ab991
KIMSEOKWON00 <150672915+KIMSEOKWON00@users.noreply.github.com>
2025-02-27 18:34:41 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import logging
 import yaml
+import os
 
 # 설정 파일 경로
 config_file = 'bitenews.yml'
@@ -9,7 +10,12 @@ def load_config(file_path):
     with open(file_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
+
+# 환경변수에서 시크릿 값 불러오기 (없으면 설정 파일에서 가져오기)
 config = load_config(config_file)
+config["AWS_ACCESS_KEY_ID"] = os.environ.get("AWS_ACCESS_KEY_ID", config.get("AWS_ACCESS_KEY_ID"))
+config["AWS_SECRET_ACCESS_KEY"] = os.environ.get("AWS_SECRET_ACCESS_KEY", config.get("AWS_SECRET_ACCESS_KEY"))
+config["OPENAI_API_KEY"] = os.environ.get("OPENAI_API_KEY", config.get("OPENAI_API_KEY"))
 
 # 로그 설정
 log_format = '%(levelname)s: %(asctime)s - %(name)s - %(message)s'
@@ -26,3 +32,5 @@ console_handler.setFormatter(formatter)
 
 # 핸들러 추가
 logger.addHandler(console_handler)
+
+logger.info("Configuration loaded successfully.")
